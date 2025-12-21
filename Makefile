7a3c70403a89952ecd300ba6bdc6db17b0f21e37
Max Aitken <maxaitken472@gmail.com>
2025-12-22 00:13:09 +1000

    Chapter 3.14 - Added initial representation of bytecode:

---
+++ Makefile
@@ -0,0 +1,21 @@
+CC      = gcc
+CFLAGS  = -std=c99 -Wall -Wextra
+TARGET  = clox
+
+SRC = \
+  main.c \
+  chunk.c \
+  memory.c \
+  debug.c \
+	value.c
+
+OBJ = $(SRC:.c=.o)
+
+$(TARGET): $(OBJ)
+	$(CC) $(CFLAGS) -o $(TARGET) $(OBJ)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ) $(TARGET)
---
+++ memory.h
@@ -0,0 +1,17 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) \
+  ((capacity) < 8 ? 8 : 2 * capacity)
+
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
+  (type*)reallocate(pointer, sizeof(type) * oldCount, sizeof(type) * newCount)
+
+#define FREE_ARRAY(type, pointer, oldCount) \
+  reallocate(pointer, sizeof(type) * oldCount, 0)
+
+void* reallocate(void* pointer, size_t oldCount, size_t newCount);
+
+#endif
